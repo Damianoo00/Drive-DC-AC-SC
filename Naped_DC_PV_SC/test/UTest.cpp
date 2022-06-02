26ffc88a0298fdbe3dc810ad4c98cc0a9338164e
damianoo00 <damianpla.protondynamic@gmail.com>
2022-06-02 14:19:06 +0200

    rm PI calculation UTest

--- Naped_DC_PV_SC/test/UTest.cpp
+++ Naped_DC_PV_SC/test/UTest.cpp
@@ -28,24 +28,6 @@ void TestCalcCurrent()
     TEST_ASSERT_EQUAL_INT(106, CalcCurrent(1024, 47));
 }
 
-void TestCalacPI()
-{
-    /******** PI config *********/
-    constexpr float Ts = 10e3;
-    constexpr float Kr_i = 3.2593;
-    constexpr float Tr_i = 4.6136;
-    constexpr int8_t max_i = 1;
-    constexpr int8_t min_i = -1;
-    struct PICTRL PIctrl;
-    InitPIctrl(&PIctrl, Ts, Kr_i, Tr_i, max_i, min_i);
-
-    /********* TESTS ******************/
-    CalcPIctrl(&PIctrl, 1);
-    TEST_ASSERT_EQUAL_FLOAT(1, PIctrl.y);
-    CalcPIctrl(&PIctrl, 0.0004);
-    TEST_ASSERT_EQUAL_FLOAT(1, PIctrl.y);
-}
-
 void TestGetCiukDuty()
 {
     TEST_ASSERT_EQUAL_INT(0, GetCukDuty(0, 0));
@@ -73,7 +55,6 @@ int main(int argc, char **argv)
 
     RUN_TEST(TestCalcVoltage);
     RUN_TEST(TestCalcCurrent);
-    RUN_TEST(TestCalacPI);
     RUN_TEST(TestGetCiukDuty);
     RUN_TEST(TestRpm2rads);
     RUN_TEST(TestCalcDac);
